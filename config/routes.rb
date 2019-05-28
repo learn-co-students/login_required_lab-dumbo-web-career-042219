474b25ca9f4d9e3d30ddae2b36a0c59d5caf49f5
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-05-27 20:05:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :sessions
+
+  get '/secrets', to: 'secrets#show'
+  get 'sessions', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
